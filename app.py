1bcae9a94b92277358b5d78b1cabc3c7b56d2bc6
jainish-thakkar <136271415+jainish-thakkar@users.noreply.github.com>
2024-12-17 16:07:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-import streamlit as st
+from flask import Flask,request,jsonify
 import requests
 
-def fetch_conversion_factor(source, target):
-    url = f"https://free.currconv.com/api/v7/convert?q={source}_{target}&compact=ultra&apiKey=9aa0c54f5ad4c460c36d"
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
+    final_amount = amount * cf
+    final_amount = round(final_amount,2)
+    response = {
+        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+    }
+    return jsonify(response)
+
+def fetch_conversion_factor(source,target):
+
+    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9aa0c54f5ad4c460c36d".format(source,target)
+
     response = requests.get(url)
     response = response.json()
-    return response[f'{source}_{target}']
 
-def main():
-    st.title("Currency Converter")
-    
-    source_currency = st.text_input("Enter source currency (e.g., USD):")
-    target_currency = st.text_input("Enter target currency (e.g., EUR):")
-    amount = st.number_input("Enter amount:", min_value=0.0)
+    return response['{}_{}'.format(source,target)]
 
-    if st.button("Convert"):
-        if source_currency and target_currency and amount:
-            conversion_factor = fetch_conversion_factor(source_currency, target_currency)
-            final_amount = round(amount * conversion_factor, 2)
-            st.write(f"{amount} {source_currency} is {final_amount} {target_currency}")
-        else:
-            st.write("Please fill all fields.")
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
